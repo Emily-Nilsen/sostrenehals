6264b19667fc8c2d5b01a9e93a50b280ce96832e
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-09-12 12:55:54 +0200

    Scrollable?

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -5,37 +5,37 @@ import { motion } from 'framer-motion'
 import { useRef, useEffect, useState } from 'react'
 
 export function PhotoGallery() {
-  const [width, setWidth] = useState(0)
-  const carousel = useRef()
+  // const [width, setWidth] = useState(0)
+  // const carousel = useRef()
 
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-  }, [])
+  // useEffect(() => {
+  //   setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  // }, [])
   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
   return (
     <motion.div
-      ref={carousel}
-      whileTap={{ cursor: 'grabbing' }}
-      className="w-full h-full overflow-hidden cursor-grab"
+    // ref={carousel}
+    // whileTap={{ cursor: 'grabbing' }}
+    // className="w-full h-full overflow-hidden cursor-grab"
     >
       <motion.div
-        drag="x"
-        dragConstraints={{ right: 0, left: -width }}
-        className="mt-4 mb-20 w-[150vw] overflow-hidden sm:mt-6 sm:mb-24"
+        // drag="x"
+        // dragConstraints={{ right: 0, left: -width }}
+        className="w-screen mt-4 mb-20 overflow-scroll sm:mt-6 sm:mb-24"
       >
-        <div className="flex justify-center gap-5 py-4 -my-4 overflow-hidden sm:gap-8">
+        <div className="flex justify-center gap-5 py-4 -my-4 overflow-scroll sm:gap-8">
           {[
             'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image1_ijhoiz.jpg',
             'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image2_bp8unj.jpg',
             'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image3_mjp8nv.jpg',
             'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image4_z4kj1w.jpg',
-            'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image1_ijhoiz.jpg',
+            'https://res.cloudinary.com/dt3k2apqd/image/upload/v1662744958/S%C3%B8strene%20Hals/image2_bp8unj.jpg',
           ].map((image, imageIndex) => (
             <div
               key={image.src}
               className={clsx(
-                'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                'relative aspect-[9/10] w-44 flex-none overflow-scroll rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
                 rotations[imageIndex % rotations.length]
               )}
             >
