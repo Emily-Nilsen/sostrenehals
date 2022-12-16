4b3f6ed3e93e7cf715d37836296389a97a6b398f
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-12-16 08:19:28 +0100

    Remove modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ export default function Home() {
           <PhotoGallery />
           <Contact />
           <PhotoCollage />
-          <Modal />
+          {/* <Modal /> */}
         </>
       </main>
       <Footer />
