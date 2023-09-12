cafc0eea4a160511c6e42d82ad825b3c7b20cd72
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-09-12 11:05:24 +0200

    add julekonsert modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,11 +21,11 @@ export function Modal() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 transition-opacity bg-opacity-75 bg-nutmeg/60" />
+            <div className="fixed inset-0 bg-nutmeg/60 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -35,21 +35,21 @@ export function Modal() {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative px-4 pt-5 pb-4 m-3 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:p-6">
+                <Dialog.Panel className="relative m-3 transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:p-6">
                   <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
                     <button
                       type="button"
-                      className="transition duration-300 ease-in-out bg-white rounded-md text-nutmeg hover:text-couture-darkGold focus:outline-none focus:ring-2 focus:ring-nutmeg/50 focus:ring-offset-2"
+                      className="rounded-md bg-white text-nutmeg transition duration-300 ease-in-out hover:text-couture-darkGold focus:outline-none focus:ring-2 focus:ring-nutmeg/50 focus:ring-offset-2"
                       onClick={() => setOpen(false)}
                     >
                       <span className="sr-only">Close</span>
-                      <XMarkIcon className="w-6 h-6" aria-hidden="true" />
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
                   </div>
-                  <div className="w-full h-full md:max-w-sm">
-                    <div className="flex items-center justify-center w-full h-full mx-auto mb-4 overflow-hidden rounded-lg bg-nutmeg md:mt-10">
+                  <div className="h-full w-full md:max-w-sm">
+                    <div className="mx-auto mb-4 flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-nutmeg md:mt-10">
                       <Image
-                        src="https://res.cloudinary.com/dyumdt5s7/image/upload/v1668514364/general/Julekonsert_i_Ris_kirke_bh4bse.webp"
+                        src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/S%C3%B8strene%20Hals/Julekonsert_-_kun_dato_-_modal_stcshw.webp"
                         alt="Julekonsert i Ris kirke, 2022"
                         width={3508}
                         height={4961}
@@ -62,11 +62,11 @@ export function Modal() {
                     <a
                       rel="noopener noreferrer"
                       target="_blank"
-                      href="https://www.ticketmaster.no/event/julekonsert-med-sostrene-hals-i-ris-kirke-med-gjester-tickets/702367?language=en-us"
+                      href="https://www.ticketmaster.no/event/julekonsert-med-sostrene-hals-og-solvguttene-billetter/725651"
                     >
                       <button
                         type="button"
-                        className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white transition duration-300 ease-in-out border border-transparent rounded-md shadow-sm bg-lipstick-dark shadow-lipstick-dark/10 hover:contrast-125 focus:outline-none focus:ring-2 focus:ring-lipstick-light focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-lipstick-dark px-4 py-2 text-base font-medium text-white shadow-sm shadow-lipstick-dark/10 transition duration-300 ease-in-out hover:contrast-125 focus:outline-none focus:ring-2 focus:ring-lipstick-light focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                         onClick={() => setOpen(false)}
                       >
                         {t('common:buy')}
@@ -74,7 +74,7 @@ export function Modal() {
                     </a>
                     <button
                       type="button"
-                      className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium transition duration-300 ease-in-out bg-white border rounded-md shadow-sm border-nutmeg/30 text-couture-darkGold/80 shadow-nutmeg/5 hover:border-nutmeg/50 hover:text-couture-darkGold focus:outline-none focus:ring-2 focus:ring-nutmeg/50 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-nutmeg/30 bg-white px-4 py-2 text-base font-medium text-couture-darkGold/80 shadow-sm shadow-nutmeg/5 transition duration-300 ease-in-out hover:border-nutmeg/50 hover:text-couture-darkGold focus:outline-none focus:ring-2 focus:ring-nutmeg/50 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
                       onClick={() => setOpen(false)}
                     >
                       {t('common:close')}
