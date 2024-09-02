9f09673fa08402958904078ac8f2ff56078a6d01
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-09-02 16:01:00 +0300

    add spotify component

---
+++ src/components/SpotifyPlayer.jsx
@@ -0,0 +1,17 @@
+export function SpotifyPlayer({ spotifyUri }) {
+  const spotifyEmbedUrl = `https://open.spotify.com/embed/${spotifyUri}`
+
+  return (
+    <div className="mx-left w-full max-w-xl overflow-hidden rounded-xl">
+      <iframe
+        src={spotifyEmbedUrl}
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+        className="shadow-lg"
+      ></iframe>
+    </div>
+  )
+}
