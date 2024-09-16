cdab223ef29e574de566b07f79d55ccbe00f6eed
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-09-16 18:45:16 +0300

    update config file

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
 
-// const nextConfig = {
-//   reactStrictMode: true,
-//   experimental: {
-//     newNextLinkBehavior: true,
-//     scrollRestoration: true,
-//     images: {
-//       allowFutureImage: true,
-//     },
-//   },
-//   images: {
-//     minimumCacheTTL: 31536000,
-//     domains: ['res.cloudinary.com'],
-//   },
-// }
-
 module.exports = {
   ...nextTranslate(),
   reactStrictMode: true,
@@ -28,6 +13,5 @@ module.exports = {
   },
   images: {
     minimumCacheTTL: 31536000,
-    domains: ['res.cloudinary.com'],
   },
 }
